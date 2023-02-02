bc6e11531f7dcbba51863af78e66f7cecec6adef
zhangsetsail <zqh15131121078@126.com>
2023-02-02 16:06:52 +0800

    feat: dapr test cooperate with gin framework

--- dapr_study/dapr1/main.go
+++ dapr_study/dapr1/main.go
@@ -4,33 +4,75 @@ import (
 	"context"
 	"fmt"
 	dapr "github.com/dapr/go-sdk/client"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+type DaprTest struct {
+	client dapr.Client
+	ctx    context.Context
+	store  string
+}
+
+type KeyValue struct {
+	Key   string `form:"key"`
+	Value string `form:"value"`
+}
+
+func (d DaprTest) Add(c *gin.Context) {
+	var kv KeyValue
+	if err := c.ShouldBindQuery(&kv); err != nil {
+		c.Error(err)
+	}
+	data := []byte(kv.Value)
+	if err := d.client.SaveState(d.ctx, d.store, kv.Key, data, nil); err != nil {
+		c.Error(err)
+	}
+	c.String(http.StatusOK, "Successfully added")
+}
+
+func (d DaprTest) Get(c *gin.Context) {
+	var kv KeyValue
+	if err := c.ShouldBindQuery(&kv); err != nil {
+		c.Error(err)
+	}
+	item, err := d.client.GetState(d.ctx, d.store, kv.Key, nil)
+	if err != nil {
+		c.Error(err)
+	}
+	ret := fmt.Sprintf("value is %v", string(item.Value))
+	c.String(http.StatusOK, ret)
+}
+
+func (d DaprTest) Delete(c *gin.Context) {
+	var kv KeyValue
+	if err := c.ShouldBindQuery(&kv); err != nil {
+		c.Error(err)
+	}
+	if err := d.client.DeleteState(d.ctx, d.store, kv.Key, nil); err != nil {
+		c.Error(err)
+	}
+	c.String(http.StatusOK, "Successfully delete")
+}
+
+func initDapr() DaprTest {
+	var err error
+	var daprCli DaprTest
+	daprCli.client, err = dapr.NewClient()
+	daprCli.ctx = context.Background()
+	daprCli.store = "statestore"
+	if err != nil {
+		panic(err)
+	}
+	return daprCli
+}
+
 func main() {
-	client, err := dapr.NewClient()
-	if err != nil {
-		panic(err)
-	}
-	defer client.Close()
-
-	ctx := context.Background()
-	data := []byte("hello")
-	store := "statestore" // defined in the component YAML
-
-	// save state with the key key1, default options: strong, last-write
-	if err := client.SaveState(ctx, store, "key1", data, nil); err != nil {
-		panic(err)
-	}
-
-	// get state for key key1
-	item, err := client.GetState(ctx, store, "key1", nil)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("data [key:%s etag:%s]: %s", item.Key, item.Etag, string(item.Value))
-
-	// delete state for key key1
-	if err := client.DeleteState(ctx, store, "key1", nil); err != nil {
-		panic(err)
-	}
+	r := gin.Default()
+	daprCli := initDapr()
+	defer daprCli.client.Close()
+	r.POST("/add", daprCli.Add)
+	r.GET("/get", daprCli.Get)
+	r.DELETE("/delete", daprCli.Delete)
+	r.Run()
 }
